15aee5e4729945a55fb5d870826de0727791838a
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-07-15 10:37:52 +0300

    feat: update ButtonProps interface and modify Button usage in Landing component

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,8 +1,6 @@
 
-interface ButtonProps {
-  title: string;
-  styles?: string;
-}
+import { ButtonProps } from "@/interfaces";
+
 const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
     <button className={`px-4 py-2 font-medium ${styles}`}>
